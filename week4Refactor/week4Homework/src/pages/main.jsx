3a9c0c05180f15b308c9eef3139753d4fdcd27ad
JeeminYi <yjjmmm000@g.skku.edu>
2024-06-12 01:46:46 +0900

    feat: 메인 페이지 구현

---
+++ week4Refactor/week4Homework/src/pages/main.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import Jeemin from "../assets/Jeemin.svg";
+import { useNavigate } from "react-router-dom";
+
+const Main = () => {
+  const navigate = useNavigate();
+  return (
+    <MainPageWrapper>
+      <HeaderSection>축하합니달라~💸</HeaderSection>
+      <Img src={Jeemin} />
+      <BtnSection>
+        <Btn onClick={() => navigate("/My")}>내 정보</Btn>
+        <Btn onClick={() => navigate("/Join")}>회원가입</Btn>
+      </BtnSection>
+    </MainPageWrapper>
+  );
+};
+
+export default Main;
+
+const MainPageWrapper = styled.div`
+  background-color: #fffbe9;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const HeaderSection = styled.header`
+  position: fixed;
+  width: 100%;
+  height: 5rem;
+  align-content: center;
+  background-color: black;
+  color: white;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
+  top: 0;
+  z-index: 2;
+`;
+
+const Img = styled.img`
+  width: 30rem;
+  margin-bottom: 3.5rem;
+`;
+
+const BtnSection = styled.span`
+  width: 20rem;
+  gap: 3rem;
+  display: flex;
+  margin-top: 6rem;
+`;
+
+const Btn = styled.button`
+  width: 8rem;
+  height: 3rem;
+  border-radius: 0.8rem;
+  background-color: black;
+  color: white;
+  align-items: center;
+  align-content: center;
+  font-size: 12px;
+  font-weight: 400;
+`;
--- week4Refactor/week4Homework/src/router.jsx
+++ week4Refactor/week4Homework/src/router.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Join from "./pages/join";
 import LogIn from "./pages/logIn";
+import Main from "./pages/main";
+
 const Router = () => {
   return (
     <BrowserRouter>
@@ -8,6 +10,7 @@ const Router = () => {
         <Route path="/" element={<Join />}></Route>
         <Route path="/LogIn" element={<LogIn />}></Route>
         <Route path="/Join" element={<Join />}></Route>
+        <Route path="/Main" element={<Main />}></Route>
       </Routes>
     </BrowserRouter>
   );
